8d946dc468d1fcb99e5d3e1eb3318315c23a05c9
PiciAkk <marci@dbx.hu>
2021-08-13 19:10:24 +0200

    big update

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+function sleep(sleepDuration){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){ /* do nothing */ }
+}
+function showHoverMenu() {
+  var menu = document.getElementById("menu");
+  menu.style.display = "block";
+}
+function hideHoverMenu() {
+  var menu = document.getElementById("menu");
+  menu.style.display = "none";
+}
